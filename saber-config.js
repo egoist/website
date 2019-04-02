ed542616d7801e893e91a3adb2048d4724e75bf9
EGOIST <0x142857@gmail.com>
2019-04-02 19:28:00 +0800

    add ga

--- saber-config.js
+++ saber-config.js
@@ -1,5 +1,13 @@
 module.exports = {
   css: {
     extract: process.env.NODE_ENV === 'production'
-  }
+  },
+  plugins: [
+    {
+      resolve: 'saber-plugin-google-analytics',
+      options: {
+        trackId: 'UA-54857209-17'
+      }
+    }
+  ]
 }
