829db8351adcc5dac33c821b985cd57fc2668a0b
EGOIST <0x142857@gmail.com>
2021-11-08 15:14:46 +0800

    remove loader-utils

--- scripts/frontmatter-loader.ts
+++ scripts/frontmatter-loader.ts
@@ -1,11 +1,10 @@
-import { getOptions } from 'loader-utils'
 import { LoaderDefinitionFunction } from 'webpack'
 import devalue from 'devalue'
 import { parseContent } from './build-pages'
 
 const loader: LoaderDefinitionFunction = async function (source) {
-  const options = getOptions(this)
-  if (options.type === 'pre') {
+  const { type } = this.query as { type: string }
+  if (type === 'pre') {
     const parsed = await parseContent(this.resourcePath, source)
     return `export var frontmatter = ${devalue(parsed.frontmatter)};
 ${parsed.content}`
--- types.d.ts
+++ types.d.ts
@@ -2,5 +2,3 @@ declare module '$pages' {
   const pages: any[]
   export default pages
 }
-
-declare module 'loader-utils'
