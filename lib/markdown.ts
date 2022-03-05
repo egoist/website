975c7d2b4c21d0e4f9c19304e1adc267886ae0b4
EGOIST <0x142857@gmail.com>
2022-03-05 22:18:56 +0800

    linkify

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,7 +1,7 @@
 import MarkdownIt from "markdown-it"
 
 export const renderMarkdown = async (content: string) => {
-  const md = new MarkdownIt({ html: true })
+  const md = new MarkdownIt({ html: true, linkify: true })
   const html = md.render(content)
   return html
 }
