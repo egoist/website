3ca5b3733377ebfad317986fa4cbece3c23ac828
EGOIST <hi@egoist.sh>
2022-05-04 16:37:10 +0800

    tweaks

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
-import Script from "next/script"
 import { site } from "~/config"
-
-const PROD = process.env.NODE_ENV === "production"
 
 export default function Document() {
   return (
@@ -25,12 +22,12 @@ export default function Document() {
       <body className="dark:bg-dark dark:text-zinc-200">
         <Main />
         <NextScript />
-        {PROD && (
-          <Script
-            strategy="afterInteractive"
+        {process.env.NODE_ENV === "production" && (
+          <script
+            async
             data-website-id="41199b15-9e7e-4354-8e74-95d4c48e178e"
             src="https://umami.egoist.sh/mami.js"
-          ></Script>
+          ></script>
         )}
       </body>
     </Html>
