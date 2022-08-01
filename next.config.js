32cd5770e7e31b95eda5dbf221d69914966612e9
EGOIST <0x142857@gmail.com>
2022-08-01 18:41:44 +0800

    disable preact

--- next.config.js
+++ next.config.js
@@ -37,13 +37,13 @@ module.exports = {
   },
   webpack(config, { dev, isServer }) {
     // Replace React with Preact in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      })
-    }
+    // if (!dev && !isServer) {
+    //   Object.assign(config.resolve.alias, {
+    //     react: "preact/compat",
+    //     "react-dom/test-utils": "preact/test-utils",
+    //     "react-dom": "preact/compat",
+    //   })
+    // }
 
     return config
   },
