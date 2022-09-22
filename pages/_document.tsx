81c8a28d4a698c75c40a533552f2b57912d390b7
EGOIST <0x142857@gmail.com>
2022-09-22 18:37:04 +0800

    tweaks

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document"
 
+const PROD = process.env.NODE_ENV === "production"
+
 export default function Document() {
   return (
     <Html>
@@ -22,21 +24,23 @@ export default function Document() {
         />
         {/** json feed */}
         <link rel="alternate" type="application/json" href="/feed.json" />
+        {PROD && (
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4882368575250522"
+            crossOrigin="anonymous"
+          ></script>
+        )}
       </Head>
       <body className="">
         <Main />
         <NextScript />
-        {process.env.NODE_ENV === "production" && (
+        {PROD && (
           <>
             <script
               async
               data-website-id="c83fb747-c4bd-42e8-9668-b2790be38b67"
               src="https://umami.egoist.dev/mami.js"
-            ></script>
-            <script
-              async
-              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4882368575250522"
-              crossOrigin="anonymous"
             ></script>
           </>
         )}
