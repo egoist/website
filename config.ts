d653090ec6e50cec3d17a86310af7aabd6a47001
EGOIST <hi@egoist.dev>
2023-02-09 21:31:53 +0800

    tweaks

--- config.ts
+++ config.ts
@@ -1,13 +1,13 @@
 export const site = {
   title: "EGOIST",
   headerTitle: "EGOIST",
-  description: `The coolest anime fan`,
+  description: `This is a genius description`,
   url: "https://egoist.dev",
-  twitter: "_egoistlily",
+  twitter: "poorlybatched",
   author: "EGOIST",
   email: "hi@egoist.dev",
   disableSignup: true,
   github: "egoist",
-  telegram: "definitelymid",
+  telegram: "egoistsama",
   youtube: "https://www.youtube.com/channel/UCKhaJ86HV7zsklPaCRxD_4A",
 }
